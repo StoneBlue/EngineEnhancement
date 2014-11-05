187c39348f47b27129e9bdc1c2df87b5b83f565e
MOARdV <cwebb@babylonbycandlelight.com>
2014-11-05 13:49:47 -0600

    Initial source commit
    v0.2 code

---
+++ EngineAnimation.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EngineEnhancement
+{
+    public class EngineAnimation : PartModule
+    {
+        // Are we going to play the animation in reverse?
+        [KSPField(isPersistant = true)]
+        private bool reverseAnimation = false;
+
+        [KSPField(isPersistant = true)]
+        public string onActivateAnimation = null;
+
+        [KSPField(isPersistant = true)]
+        public float customAnimationSpeed = 1.0f;
+
+        private bool playReverseAnimation = false;
+        private bool enginesFound = false;
+        private bool initialized = false;
+        private Animation engineAnimation = null;
+
+        private MultiModeEngine mmeModule = null;
+        private List<ModuleEngines> engineModules = new List<ModuleEngines>();
+        private List<ModuleEnginesFX> engineFxModules = new List<ModuleEnginesFX>();
+
+        private void FindEngines()
+        {
+            if (!enginesFound && part != null)
+            {
+                foreach (PartModule thatModule in part.Modules)
+                {
+                    var mme = thatModule as MultiModeEngine;
+                    if (mme != null)
+                    {
+                        mmeModule = mme;
+                    }
+                    var em = thatModule as ModuleEngines;
+                    if(em != null)
+                    {
+                        engineModules.Add(em);
+                    }
+                    var efm = thatModule as ModuleEnginesFX;
+                    if(efm != null)
+                    {
+                        engineFxModules.Add(efm);
+                    }
+                }
+
+                enginesFound = true;
+            }
+        }
+
+        private bool NeedUpdateAnimation()
+        {
+            bool engineIgnited = false;
+            if (mmeModule != null)
+            {
+                string activeEngine = (mmeModule.runningPrimary) ? mmeModule.primaryEngineID : mmeModule.secondaryEngineID;
+
+                foreach (ModuleEnginesFX engine in engineFxModules)
+                {
+                    if (engine.name == activeEngine && engine.EngineIgnited)
+                    {
+                        engineIgnited = true;
+                    }
+                }
+            }
+            else
+            {
+                // What to do if there are multiple engine modules?  For now,
+                // treat it as "any engine that's on".
+                foreach (ModuleEngines engine in engineModules)
+                {
+                    //Debug.Log("EMME - Need Update: engineShutdown = " + engine.engineShutdown + );
+                    if (engine.EngineIgnited)
+                    {
+                        engineIgnited = true;
+                    }
+                }
+
+                foreach (ModuleEnginesFX engine in engineFxModules)
+                {
+                    if (engine.EngineIgnited)
+                    {
+                        engineIgnited = true;
+                    }
+                }
+            }
+
+            return (playReverseAnimation == (reverseAnimation ^ engineIgnited));
+        }
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            base.OnStart(state);
+
+            FindEngines();
+
+            if (enginesFound && !initialized)
+            {
+                initialized = true;
+                if (onActivateAnimation != null)
+                {
+                    engineAnimation = part.FindModelAnimators(onActivateAnimation).FirstOrDefault();
+                }
+            }
+
+            if (engineAnimation != null)
+            {
+                // Set the initial state of the animation
+                playReverseAnimation = (engineAnimation[onActivateAnimation].normalizedTime < 0.001f) ^ reverseAnimation;
+                // Unfortunately, the side effect is that the animation plays on start if it's out of whack.
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            FindEngines();
+
+            if (enginesFound && engineAnimation != null && NeedUpdateAnimation())
+            {
+                playReverseAnimation = !playReverseAnimation;
+
+                UpdateAnimation();
+            }
+        }
+
+        private void UpdateAnimation()
+        {
+            if (playReverseAnimation)
+            {
+                engineAnimation[onActivateAnimation].speed = -1f * customAnimationSpeed;
+                if (engineAnimation[onActivateAnimation].normalizedTime == 0f || engineAnimation[onActivateAnimation].normalizedTime == 1f)
+                {
+                    engineAnimation[onActivateAnimation].normalizedTime = 1f;
+                }
+            }
+            else
+            {
+                engineAnimation[onActivateAnimation].speed = 1f * customAnimationSpeed;
+                if (engineAnimation[onActivateAnimation].normalizedTime == 0f || engineAnimation[onActivateAnimation].normalizedTime == 1f)
+                {
+                    engineAnimation[onActivateAnimation].normalizedTime = 0f;
+                }
+            }
+
+            engineAnimation.Play(onActivateAnimation);
+        }
+    }
+}
---
+++ ModuleRestrictedGimbal.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EngineEnhancement
+{
+    class ModuleRestrictedGimbal:ModuleGimbal
+    {
+        [KSPField(isPersistant = false)]
+        public float pitchRange = 90.0f;
+
+        [KSPField(isPersistant = false)]
+        public float rollRange = 90.0f;
+
+        [KSPField(isPersistant = false)]
+        public float yawRange = 90.0f;
+
+        private bool showedError = false;
+
+        // ModuleGimbal
+        //public float gimbalAnglePitch;
+        //public float gimbalAngleRoll;
+        //public float gimbalAngleYaw;
+        //[KSPField(isPersistant = true)]
+        //public bool gimbalLock;
+        //[KSPField(isPersistant = false)]
+        //public float gimbalRange;
+        //[KSPField(isPersistant = false)]
+        //public float gimbalResponseSpeed;
+        //[KSPField(isPersistant = false)]
+        //public string gimbalTransformName;
+        //public List<UnityEngine.Transform> gimbalTransforms;
+        //public List<UnityEngine.Quaternion> initRots;
+        //[KSPField(isPersistant = false)]
+        //public bool useGimbalResponseSpeed;
+
+        public override string GetInfo()
+        {
+            return string.Format("Maximum Pitch: {0}°\nMaximum Yaw: {1}°", pitchRange, yawRange);
+        }
+
+        public override void OnFixedUpdate()
+        {
+            if (!HighLogic.LoadedSceneIsFlight || !vessel.isActiveVessel) return;
+
+            if (gimbalTransforms.Count > initRots.Count && !showedError)
+            {
+                Debug.LogError(this.name + " - too many gimbal transforms.  Disabling module.");
+                showedError = true;
+                return;
+            }
+
+            FlightCtrlState ctrl = vessel.ctrlState;
+
+            if (useGimbalResponseSpeed)
+            {
+                float toYaw = ctrl.yaw * yawRange;
+                float toPitch = ctrl.pitch * pitchRange;
+                float toRoll = ctrl.roll * rollRange;
+                gimbalAngleYaw = Mathf.Lerp(gimbalAngleYaw, toYaw, gimbalResponseSpeed * TimeWarp.deltaTime);
+                gimbalAnglePitch = Mathf.Lerp(gimbalAnglePitch, toPitch, gimbalResponseSpeed * TimeWarp.deltaTime);
+                gimbalAngleRoll = Mathf.Lerp(gimbalAngleRoll, toRoll, gimbalResponseSpeed * TimeWarp.deltaTime);
+            }
+            else
+            {
+                gimbalAngleYaw = ctrl.yaw * yawRange;
+                gimbalAnglePitch = ctrl.pitch * pitchRange;
+                gimbalAngleRoll = ctrl.roll * rollRange;
+            }
+
+            for(int i=0; i<gimbalTransforms.Count; ++i)
+            {
+                gimbalTransforms[i].localRotation = initRots[i] * Quaternion.AngleAxis(this.gimbalAnglePitch, gimbalTransforms[i].InverseTransformDirection(vessel.ReferenceTransform.right)) * Quaternion.AngleAxis(gimbalAngleYaw, gimbalTransforms[i].InverseTransformDirection(vessel.ReferenceTransform.forward));
+            }
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+
+            pitchRange = Math.Max(0.0f, Math.Min(pitchRange, gimbalRange));
+            rollRange = Math.Max(0.0f, Math.Min(rollRange, gimbalRange));
+            yawRange = Math.Max(0.0f, Math.Min(yawRange, gimbalRange));
+        }
+    }
+}
