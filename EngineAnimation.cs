5cc115c29ccf78f4fbcdc54b89a3ccb0eeaaf0ad
MOARdV <cwebb@babylonbycandlelight.com>
2014-11-08 08:16:11 -0600

    Fixes / updates to EngineAnimation
    Handles throttle-dependent animation, throttle-dependent spinners.

--- EngineAnimation.cs
+++ EngineAnimation.cs
@@ -9,24 +9,90 @@ namespace EngineEnhancement
 {
     public class EngineAnimation : PartModule
     {
-        // Are we going to play the animation in reverse?
-        [KSPField(isPersistant = true)]
-        private bool reverseAnimation = false;
+        private class SpinnerTransformation
+        {
+            public readonly Transform transform;
+            public readonly string transformName;
+            public readonly float spinRPM;
+            private readonly int layer;
 
-        [KSPField(isPersistant = true)]
+            public SpinnerTransformation(ConfigNode node, Part part, int _layer)
+            {
+                layer = _layer;
+                if (node.HasValue("transformName"))
+                {
+                    transformName = node.GetValue("transformName").Trim();
+                }
+                else
+                {
+                    transformName = null;
+                }
+
+                spinRPM = 0.0f;
+                if (node.HasValue("spinRPM"))
+                {
+                    float.TryParse(node.GetValue("spinRPM").Trim(), out spinRPM);
+                }
+
+                if (transformName != null)
+                {
+                    transform = part.FindModelTransform(transformName);
+                    if(transform == null)
+                    {
+                        Debug.LogError("Did not find spinner transform " + transformName);
+                    }
+                }
+                else
+                {
+                    transform = null;
+                }
+            }
+
+            public void UpdateThrottle(float throttle)
+            {
+                // How many degrees did we turn?
+                float rotation = spinRPM * throttle * TimeWarp.deltaTime * 360.0f;
+                transform.Rotate(Vector3.forward * rotation);
+            }
+        }
+
+        // Are we going to play the animation in reverse?
+        [KSPField]
         public string onActivateAnimation = null;
 
-        [KSPField(isPersistant = true)]
-        public float customAnimationSpeed = 1.0f;
+        [KSPField]
+        private bool reverseActivationAnimation = false;
 
-        private bool playReverseAnimation = false;
+        [KSPField]
+        public float customActivationAnimationSpeed = 1.0f;
+
+        [KSPField]
+        public string onThrottleAnimation = null;
+
+        [KSPField]
+        private bool smoothThrottleAnimation = true;
+
+        [KSPField]
+        public float throttleResponseSpeed = 1.0f;
+
+        private bool playReverseActivationAnimation = false;
         private bool enginesFound = false;
         private bool initialized = false;
-        private Animation engineAnimation = null;
+        private Animation engineActivationAnimation = null;
+        private Animation engineThrottleAnimation = null;
 
         private MultiModeEngine mmeModule = null;
         private List<ModuleEngines> engineModules = new List<ModuleEngines>();
         private List<ModuleEnginesFX> engineFxModules = new List<ModuleEnginesFX>();
+
+        private List<SpinnerTransformation> spinnerAnimations = new List<SpinnerTransformation>();
+
+        //--- Hacktastic!  In order to keep the child ConfigNodes around so I
+        // can use them when I need them, I have to stuff a copy of my config
+        // node in here.
+        public ConfigNode myConfigNode;
+
+        // MOARdV TODO: different animation layers per animation?
 
         private void FindEngines()
         {
@@ -76,7 +142,6 @@ namespace EngineEnhancement
                 // treat it as "any engine that's on".
                 foreach (ModuleEngines engine in engineModules)
                 {
-                    //Debug.Log("EMME - Need Update: engineShutdown = " + engine.engineShutdown + );
                     if (engine.EngineIgnited)
                     {
                         engineIgnited = true;
@@ -92,66 +157,154 @@ namespace EngineEnhancement
                 }
             }
 
-            return (playReverseAnimation == (reverseAnimation ^ engineIgnited));
+            return (playReverseActivationAnimation == (reverseActivationAnimation ^ engineIgnited));
         }
 
+        //--------------------------------------------------------------------
+        // OnLoad
+        public override void OnLoad(ConfigNode cfg)
+        {
+            base.OnLoad(cfg);
+
+            if (myConfigNode == null)
+            {
+                // MOARdV: Hacktastic!  This method is called once, and it
+                // does not seem to allow you to preserve variables you
+                // initialize here, so when it comes time to instantiate a part
+                // in the VAB, anything you set up here is lost.  But, you can
+                // preserve the ConfigNode for later.
+                myConfigNode = cfg;
+            }
+        }
+
+        //--------------------------------------------------------------------
+        // OnStart
         public override void OnStart(PartModule.StartState state)
         {
             base.OnStart(state);
 
-            FindEngines();
+            Debug.Log("EE - OnStart");
 
-            if (enginesFound && !initialized)
+            if (!initialized)
             {
-                initialized = true;
-                if (onActivateAnimation != null)
+                FindEngines();
+                if (enginesFound)
                 {
-                    engineAnimation = part.FindModelAnimators(onActivateAnimation).FirstOrDefault();
+                    if (onActivateAnimation != null)
+                    {
+                        engineActivationAnimation = part.FindModelAnimators(onActivateAnimation).FirstOrDefault();
+                        if (engineActivationAnimation == null)
+                        {
+                            Debug.LogError("EE - did not find animation " + onActivateAnimation);
+                        }
+                    }
+
+                    if (onThrottleAnimation != null)
+                    {
+                        engineThrottleAnimation = part.FindModelAnimators(onThrottleAnimation).FirstOrDefault();
+                        if (engineThrottleAnimation == null)
+                        {
+                            Debug.LogError("EE - did not find animation " + onThrottleAnimation);
+                        }
+                    }
+
+                    var spinnerNodes = myConfigNode.GetNodes("ThrottleSpinner");
+                    int layer = 4;
+                    foreach (ConfigNode spinnerNode in spinnerNodes)
+                    {
+                        SpinnerTransformation spinner = new SpinnerTransformation(spinnerNode, part, layer);
+                        if(spinner.transform != null)
+                        {
+                            ++layer;
+                            spinnerAnimations.Add(spinner);
+                        }
+                    }
+
+                    initialized = true;
                 }
             }
 
-            if (engineAnimation != null)
+            if (HighLogic.LoadedSceneIsFlight)
             {
-                // Set the initial state of the animation
-                playReverseAnimation = (engineAnimation[onActivateAnimation].normalizedTime < 0.001f) ^ reverseAnimation;
-                // Unfortunately, the side effect is that the animation plays on start if it's out of whack.
+                if (engineActivationAnimation != null)
+                {
+                    // Set the initial state of the animation
+                    playReverseActivationAnimation = (engineActivationAnimation[onActivateAnimation].normalizedTime < 0.001f) ^ reverseActivationAnimation;
+                    // Unfortunately, the side effect is that the animation plays on start if it's out of whack.
+                    engineActivationAnimation[onActivateAnimation].layer = 2;
+                }
+
+                if (engineThrottleAnimation != null)
+                {
+                    engineThrottleAnimation[onThrottleAnimation].normalizedTime = vessel.ctrlState.mainThrottle;
+                    engineThrottleAnimation[onThrottleAnimation].speed = 0f;
+                    engineThrottleAnimation[onThrottleAnimation].wrapMode = WrapMode.ClampForever;
+                    engineThrottleAnimation[onThrottleAnimation].layer = 3;
+                }
             }
         }
 
         public override void OnUpdate()
         {
             base.OnUpdate();
-
-            FindEngines();
-
-            if (enginesFound && engineAnimation != null && NeedUpdateAnimation())
+            if (!HighLogic.LoadedSceneIsFlight)
             {
-                playReverseAnimation = !playReverseAnimation;
-
-                UpdateAnimation();
+                return;
             }
+
+            UpdateAnimation();
         }
 
         private void UpdateAnimation()
         {
-            if (playReverseAnimation)
+            if (engineActivationAnimation != null && NeedUpdateAnimation())
             {
-                engineAnimation[onActivateAnimation].speed = -1f * customAnimationSpeed;
-                if (engineAnimation[onActivateAnimation].normalizedTime == 0f || engineAnimation[onActivateAnimation].normalizedTime == 1f)
+                playReverseActivationAnimation = !playReverseActivationAnimation;
+
+                if (playReverseActivationAnimation)
                 {
-                    engineAnimation[onActivateAnimation].normalizedTime = 1f;
+                    engineActivationAnimation[onActivateAnimation].speed = -1f * customActivationAnimationSpeed;
+                    if (engineActivationAnimation[onActivateAnimation].normalizedTime == 0f || engineActivationAnimation[onActivateAnimation].normalizedTime == 1f)
+                    {
+                        engineActivationAnimation[onActivateAnimation].normalizedTime = 1f;
+                    }
                 }
-            }
-            else
-            {
-                engineAnimation[onActivateAnimation].speed = 1f * customAnimationSpeed;
-                if (engineAnimation[onActivateAnimation].normalizedTime == 0f || engineAnimation[onActivateAnimation].normalizedTime == 1f)
+                else
                 {
-                    engineAnimation[onActivateAnimation].normalizedTime = 0f;
+                    engineActivationAnimation[onActivateAnimation].speed = 1f * customActivationAnimationSpeed;
+                    if (engineActivationAnimation[onActivateAnimation].normalizedTime == 0f || engineActivationAnimation[onActivateAnimation].normalizedTime == 1f)
+                    {
+                        engineActivationAnimation[onActivateAnimation].normalizedTime = 0f;
+                    }
                 }
+
+                engineActivationAnimation.Play(onActivateAnimation);
             }
 
-            engineAnimation.Play(onActivateAnimation);
+            if(engineThrottleAnimation != null)
+            {
+                float goalThrottleAnimation = vessel.ctrlState.mainThrottle;
+                float throttlePosition;
+                if(smoothThrottleAnimation && engineThrottleAnimation[onThrottleAnimation].normalizedTime != goalThrottleAnimation)
+                {
+                    throttlePosition = Mathf.Lerp(engineThrottleAnimation[onThrottleAnimation].normalizedTime, goalThrottleAnimation, throttleResponseSpeed);
+                }
+                else
+                {
+                    throttlePosition = goalThrottleAnimation;
+                }
+
+                if (!engineThrottleAnimation.IsPlaying(onThrottleAnimation))
+                {
+                    engineThrottleAnimation.Play(onThrottleAnimation);
+                }
+                engineThrottleAnimation[onThrottleAnimation].normalizedTime = throttlePosition;
+            }
+
+            foreach(var spinner in spinnerAnimations)
+            {
+                spinner.UpdateThrottle(vessel.ctrlState.mainThrottle);
+            }
         }
     }
 }
